1be7abbda188057dc5df99a68989ed5a7ee2985e
Steve White <stepwhite@gmail.com>
2015-03-07 13:27:27 -0800

    Cortex-A7 "support" (copy/pasted from A8)

--- arch/arm/include/arch/arm/ops.h
+++ arch/arm/include/arch/arm/ops.h
@@ -190,7 +190,7 @@ static inline uint32_t arch_cycle_count(void)
 #else
 	return 0;
 #endif
-#elif ARM_CPU_CORTEX_A8
+#elif ARM_CPU_CORTEX_A8 || ARM_CPU_CORTEX_A7
 	uint32_t count;
 	__asm__ volatile("mrc		p15, 0, %0, c9, c13, 0"
 		: "=r" (count)
--- arch/arm/rules.mk
+++ arch/arm/rules.mk
@@ -57,6 +57,22 @@ ENABLE_THUMB := true
 ONLY_THUMB := true
 SUBARCH := arm-m
 endif
+ifeq ($(ARM_CPU),cortex-a7)
+GLOBAL_DEFINES += \
+	ARM_WITH_CP15=1 \
+	ARM_WITH_MMU=1 \
+	ARM_ISA_ARMv7=1 \
+	ARM_ISA_ARMv7A=1 \
+	ARM_WITH_VFP=1 \
+	ARM_WITH_NEON=1 \
+	ARM_WITH_THUMB=1 \
+	ARM_WITH_THUMB2=1 \
+	ARM_WITH_CACHE=1 \
+	ARM_WITH_L2=1
+GLOBAL_COMPILEFLAGS += -mcpu=$(ARM_CPU)
+HANDLED_CORE := true
+#CFLAGS += -mfpu=neon -mfloat-abi=softfp
+endif
 ifeq ($(ARM_CPU),cortex-a8)
 GLOBAL_DEFINES += \
 	ARM_WITH_CP15=1 \
